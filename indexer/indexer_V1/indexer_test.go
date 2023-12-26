da7bbd5797745c9975aa0fda3ce8c00697d5f26c
OnedGmez <oned.gomez@unah.hn>
2023-12-26 16:57:29 -0600

    ARCHIVO TEST - PARTE 2

---
+++ indexer/indexer_V1/indexer_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+)
+
+var res = make(chan string)
+
+func TestNavDir(t *testing.T) {
+	go func() {
+		for p := range emailsPath {
+			processEmail(p, &wge)
+		}
+	}()
+
+	go func() {
+		for d := range emailsData {
+			wge.Add(1)
+			if d.Id != "" {
+				j, err := json.Marshal(d)
+				if err != nil {
+					log.Println("No se pudo convertir a ndJson", err)
+				} else {
+					createIndex(j)
+				}
+			}
+			wge.Done()
+		}
+	}()
+
+	navDir(principalPath, &wge)
+	wge.Wait()
+	close(emailsData)
+}
