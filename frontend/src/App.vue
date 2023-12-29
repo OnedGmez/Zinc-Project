8ecd52fcd892779a6b96fc479b254c6bd2ba0e73
OnedGmez <oned.gomez@unah.hn>
2023-12-28 18:51:21 -0600

    CSS eliminado del archivo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="bg-red-100">
+  <header class="h-11 bg-red-100">
     <div class="ml-3.5">
         <p class="text-xl font-bold text-white">Email-Search</p>
     </div>
@@ -14,16 +14,4 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-  }
-}
 </style>
