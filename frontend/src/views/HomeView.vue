2993c54266e64c675e484c21010a8f88e8f3e2ca
OnedGmez <oned.gomez@unah.hn>
2023-12-28 18:48:16 -0600

    PARTE 3 - FUNCIONALIDAD BÁSICA (PAGINACIÓN INCOMPLETA)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,20 +1,35 @@
 <script setup>
+import { ref } from 'vue';
+
 import searchBar from '@/components/searchBar.vue';
-import TableData from '@/components/TableData.vue'
+import TableData from '@/components/TableData.vue';
+import PaginationBar from '@/components/PaginationBar.vue';
+
+var msgData = ref('')
+
+const previewMsg = (data) => {
+  msgData.value = data
+  console.log(data)
+}
 </script>
 
 <template>
-  <main>
-    <div id="search">
-      <searchBar class="mt-7 spacing w-10/12" />
-    </div>
-    <section class="mt-8">
-      <div class="flex flex-row px-4">
-        <div class="w-3/5">
-          <TableData class="spacing w-11/12"/>
+  <main class="pt-4 h-normal">
+    <section>
+      <div id="search">
+        <searchBar class="spacing w-10/12" />
+      </div>
+    </section>
+    <section class="pt-4 h-normal">
+      <div class="flex flex-row px-4 h-full">
+        <div class="w-3/5 h-full">
+          <TableData @preview="(msg) => previewMsg(msg)" class="spacing w-11/12" />
+          <PaginationBar class="h-restante z-10" />
         </div>
         <div class="w-2/5">
-          
+          <p class="whitespace-break-spaces">
+            {{msgData}}
+          </p>
         </div>
       </div>
     </section>
