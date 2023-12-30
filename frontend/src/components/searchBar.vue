47417deb5ac2aeb3ed1f4eec0a00870b38b84692
OnedGmez <oned.gomez@unah.hn>
2023-12-30 03:06:00 -0600

    PARTE 3 - Nuevo criterio de búsqueda (subject)

--- frontend/src/components/searchBar.vue
+++ frontend/src/components/searchBar.vue
@@ -1,16 +1,13 @@
 <template>
     <main>
         <div class="relative h-8">
-            <div @click="search" id="icon"
-                class="absolute z-10 bg-red-100 rounded-full h-full p-2 flex flex-wrap justify-center content-center hover:cursor-pointer">
+            <div @click="search" id="icon" class="absolute z-10 bg-red-100 rounded-full h-full p-2 flex flex-wrap justify-center content-center hover:cursor-pointer">
                 <span class="text-[white]">
                     <font-awesome-icon icon="magnifying-glass" />
                 </span>
             </div>
             <div id="input" class="absolute w-full h-full">
-                <input class="border-b border-b-red-100 rounded-full w-full h-full pl-10 focus-visible:outline-0"
-                    v-model="searching" type="text"
-                    placeholder="Buscar por remitente, destinatario o fecha">
+                <input class="border-b border-b-red-100 rounded-full w-full h-full pl-10 focus-visible:outline-0" v-model="searching" type="text" placeholder="Buscar por remitente, destinatario o fecha">
             </div>
         </div>
     </main>
@@ -28,11 +25,9 @@ var searching = ref('')
 
 const search = () => {
     var content = searching.value.toLowerCase()
-    if ((content.search("to:") >= 0) || (content.search("from:") >= 0)) {
+    if ((content.search("to:") >= 0) || (content.search("from:") >= 0) || (content.search("subject:") >= 0)) {
         var splitContent = searching.value.split(":")
-        if ((splitContent[1].trim()).length >= 4) {
-            emits('search', splitContent)
-        }
+        emits('search', splitContent)
     } else {
         //LLAMAR COMPONENTE DE ALERTA
     }
