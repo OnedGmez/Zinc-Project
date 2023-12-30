0f769f6fd36e16c24a8ba1ef8d1f5a44754ca4f3
OnedGmez <oned.gomez@unah.hn>
2023-12-30 02:59:04 -0600

    PARTE 3 - Funcionalidad completa de carga de data y paginación de la misma

--- frontend/src/components/TableData.vue
+++ frontend/src/components/TableData.vue
@@ -1,43 +1,83 @@
 <template>
-    <table class="table-fixed h-normal">
-        <thead>
-            <tr>
-                <td v-for="encabezado in encabezados" class="encabezado data-table">
-                    {{ encabezado }}
-                </td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="i in 15" @click="previewMsg()" class="special-row">
-                <td class="truncate data-table">
-                    example@prueba.com
-                </td>
-                <td class="truncate data-table">
-                    example@prueba.com, example@prueba.com, example@prueba.com, example@prueba.com
-                </td>
-                <td class="truncate data-table">
-                    Thursday 15, 2023
-                </td>
-                <td class="truncate data-table">
-                    Prueba de interfaz
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="h-normal w-full">
+        <table :class="[i >= 16 ? 'h-full': 'h-auto']" class="table-fixed w-full">
+            <thead>
+                <tr>
+                    <td v-for="encabezado in encabezados" class="encabezado data-table">
+                        {{ encabezado }}
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-if="data != ''" v-for="email in dataPages[store.actualP - 1]" @click="previewMsg(email.body)" class="special-row">
+                    <td class="truncate data-table">
+                        {{ email.to }}
+                    </td>
+                    <td class="truncate data-table">
+                        {{ email.from }}
+                    </td>
+                    <td class="truncate data-table">
+                        {{ email.date }}
+                    </td>
+                    <td class="truncate data-table">
+                        {{ email.subject }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="h-restante ">
+        <PaginationBar class="z-10" />
+    </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import PaginationBar from '@/components/PaginationBar.vue';
 
-var msg = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. \n\nQuisquam delectus, quidem magni, ipsum dicta obcaecati odit dolores expedita consequatur deleniti, vero ratione dolor atque ut mollitia in possimus hic asperiores quos quam cumque unde debitis repudiandae? Mollitia, tenetur voluptatibus magni accusamus quidem porro ipsum inventore aperiam, officia similique aliquid cum?"
+import { useDataStore } from '@/stores/data.js';
+import { ref, watchEffect } from 'vue';
+
+var msg = ''
+var data = ref([])
+var dataPages = ref([])
+var i = 1
 var encabezados = ["TO", "FROM", "DATE", "SUBJECT"]
+const store = useDataStore()
 
 const emits = defineEmits([
     'preview'
 ])
 
-const previewMsg = () => {
-    emits('preview', msg)
+const previewMsg = (data) => {
+    msg = data
+    msg = msg.replace(store.regexpS, '\n').replace(store.regexpSa, '\t')
+    emits("preview", msg)
 }
+
+watchEffect(() => {
+    if (store.accumulatedData != '') {
+        msg = ''
+        emits("preview", msg)
+        dataPages.value = []
+        i = 1
+        var dataTMP = []
+        data.value = store.accumulatedData
+
+        data.value.forEach(row => {
+            if (i % store.limit != 0) {
+                dataTMP.push(row)
+            } else {
+                dataPages.value.push(dataTMP)
+                dataTMP = []
+            }
+
+            if (i == data.value.length && dataTMP != []){
+                dataPages.value.push(dataTMP)
+                dataTMP = []
+            }
+            i++
+        });
+    }
+})
 
 </script>
