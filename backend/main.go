e89bbb6e21e6490f2ce8e17a4824577f59d6e9fa
OnedGmez <oned.gomez@unah.hn>
2023-12-30 02:52:11 -0600

    PARTE 3 - Condicionar la query para reutilizar la misma función

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -20,18 +21,26 @@ type email struct {
 var emails []email
 
 func GetEmailsData(w http.ResponseWriter, req *http.Request) {
+	var query string
 	client := http.Client{}
 	user := "ogsystem@gmail.com"
 	pass := "Complexpass#123"
 	url := "http://localhost:5080/api/default/_search"
-
-	query := `{"query": {"from": 0, "size": 1000, "sql": "SELECT * FROM \"email\" WHERE id is not null"}}`
+	criterion := req.URL.Query()["criterion"]
+	value := req.URL.Query()["value"]
+	if criterion[0] == "to" {
+		query = `{"query": {"from": 0, "size": 80000, "sql": "SELECT * FROM \"email\" WHERE` + ` \"to\" LIKE ` + `'%` + strings.TrimSpace(value[0]) + `%'"}}`
+	} else {
+		query = `{"query": {"from": 0, "size": 80000, "sql": "SELECT * FROM \"email\" WHERE` + ` \"from\" LIKE ` + `'` + strings.TrimSpace(value[0]) + `%'"}}`
+	}
+	fmt.Println(query)
 
 	r, err := http.NewRequest("POST", url, strings.NewReader(query))
 	if err != nil {
 		log.Println("No fue posible conectarse ", err)
 	} else {
 		r.SetBasicAuth(user, pass)
+		w.Header().Add("Access-Control-Allow-Origin", "*")
 		response, err := client.Do(r)
 		if err != nil {
 			log.Println("Sin respuesta ", err)
