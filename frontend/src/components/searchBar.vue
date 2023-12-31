f3146f28d3effff8c285169808dab6454f5a2f44
OnedGmez <oned.gomez@unah.hn>
2023-12-30 19:15:32 -0600

    PARTE - 3 Incorporación de alerta de error

--- frontend/src/components/searchBar.vue
+++ frontend/src/components/searchBar.vue
@@ -7,13 +7,15 @@
                 </span>
             </div>
             <div id="input" class="absolute w-full h-full">
-                <input class="border-b border-b-red-100 rounded-full w-full h-full pl-10 focus-visible:outline-0" v-model="searching" type="text" placeholder="Buscar por remitente, destinatario o fecha">
+                <input class="border-b border-b-red-100 rounded-full w-full h-full pl-10 focus-visible:outline-0" v-model="searching" type="text" placeholder="Buscar por remitente, destinatario o asunto">
             </div>
         </div>
     </main>
+    <Alert :msg="msg" v-if="alert" class="alert"/>
 </template>
 
 <script setup>
+import Alert from './Alert.vue';
 import { useDataStore } from '@/stores/data.js';
 import { ref } from 'vue';
 
@@ -21,15 +23,28 @@ const emits = defineEmits([
     'search'
 ])
 
+var alert = ref(false)
 var searching = ref('')
+var msg = ''
 
 const search = () => {
     var content = searching.value.toLowerCase()
-    if ((content.search("to:") >= 0) || (content.search("from:") >= 0) || (content.search("subject:") >= 0)) {
-        var splitContent = searching.value.split(":")
-        emits('search', splitContent)
-    } else {
-        //LLAMAR COMPONENTE DE ALERTA
+    if (content != '') {
+        if ((content.search("to:") >= 0) || (content.search("from:") >= 0) || (content.search("subject:") >= 0)) {
+            var splitContent = searching.value.split(":")
+            emits('search', splitContent)
+        } else {
+            msg = "Antepón el criterio de búsqueda (TO:, FROM: o SUBJECT:)"
+            viewAlert()
+        }
+    }else{
+        msg = "Indica lo que deseas buscar"
+        viewAlert()
     }
+}
+
+const viewAlert = () =>{
+    alert.value = !alert.value
+    setTimeout(() => { alert.value = !alert.value; }, 3600);
 }
 </script>
