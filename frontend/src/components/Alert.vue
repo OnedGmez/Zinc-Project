7b2f1f15ba013ac34d85e8cfc8fdb1c770e25d57
OnedGmez <oned.gomez@unah.hn>
2023-12-30 19:15:09 -0600

    PARTE - 3 Alerta de error

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex">
+        <span class="block mr-3"><font-awesome-icon icon="circle-info" /></span>
+        <span class="block font-semibold"> {{ msg }} </span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps([
+    'msg'
+])
+</script>
