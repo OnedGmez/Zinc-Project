6567013112c9e0aa4e014e26d82c6455f4c1214a
OnedGmez <oned.gomez@unah.hn>
2023-12-30 18:13:59 -0600

    PARTE 3 - Backend con endpoints del CRUD. (COMPLETA)

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -18,13 +19,43 @@ type email struct {
 	Date    string
 }
 
-var emails []email
+var (
+	client = http.Client{}
+	query  string
+	emails []email
+)
 
+const (
+	user = "ogsystem@gmail.com"
+	pass = "Complexpass#123"
+)
+
+/*
+PostEmailsData función de endpoint utilizada para enviar los datos a almacenar en openobserve
+
+@Versión: 1.0
+*/
+func PostEmailsData(w http.ResponseWriter, req *http.Request) {
+	url := "http://localhost:5080/api/default/email/_json"
+	content, _ := io.ReadAll(req.Body)
+	r, err := http.NewRequest("POST", url, bytes.NewBuffer(content))
+	if err != nil {
+		log.Println("No fue posible conectarse ", err)
+	} else {
+		r.SetBasicAuth(user, pass)
+		response, err := client.Do(r)
+		if err != nil {
+			fmt.Println("Sin respuesta ", err)
+		} else {
+			defer response.Body.Close()
+		}
+	}
+}
+
+/*
+GetEmailsData función de endpoint utilizada para extraer los datos que necesitamos desde Openobserve
+*/
 func GetEmailsData(w http.ResponseWriter, req *http.Request) {
-	var query string
-	client := http.Client{}
-	user := "ogsystem@gmail.com"
-	pass := "Complexpass#123"
 	url := "http://localhost:5080/api/default/_search"
 	criterion := req.URL.Query()["criterion"]
 	value := req.URL.Query()["value"]
@@ -33,8 +64,6 @@ func GetEmailsData(w http.ResponseWriter, req *http.Request) {
 	} else {
 		query = `{"query": {"from": 0, "size": 80000, "sql": "SELECT * FROM \"email\" WHERE` + ` \"from\" LIKE ` + `'` + strings.TrimSpace(value[0]) + `%' ORDER BY id ASC"}}`
 	}
-	fmt.Println(query)
-
 	r, err := http.NewRequest("POST", url, strings.NewReader(query))
 	if err != nil {
 		log.Println("No fue posible conectarse ", err)
@@ -45,7 +74,35 @@ func GetEmailsData(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			log.Println("Sin respuesta ", err)
 		} else {
-			body, err := ioutil.ReadAll(response.Body)
+			body, err := io.ReadAll(response.Body)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				mapResponse := make(map[string]interface{})
+				json.Unmarshal(body, &mapResponse)
+				json.NewEncoder(w).Encode(mapResponse)
+			}
+			defer response.Body.Close()
+		}
+	}
+}
+
+/*
+DelEmailsData función de endpoint utilizada para eliminar el stream de datos
+*/
+func DelEmailsData(w http.ResponseWriter, req *http.Request) {
+	url := "http://localhost:5080/api/default/email"
+	r, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Println("No fue posible conectarse ", err)
+	} else {
+		r.SetBasicAuth(user, pass)
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		response, err := client.Do(r)
+		if err != nil {
+			log.Println("Sin respuesta ", err)
+		} else {
+			body, err := io.ReadAll(response.Body)
 			if err != nil {
 				log.Fatal(err)
 			} else {
@@ -61,7 +118,9 @@ func GetEmailsData(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := chi.NewRouter()
 
-	router.Get("/emails", GetEmailsData)
+	router.Get("/Getemails", GetEmailsData)
+	router.Delete("/Delemails", DelEmailsData)
+	router.Post("/Postemails", PostEmailsData)
 
 	log.Println(http.ListenAndServe(":3000", router))
 }
